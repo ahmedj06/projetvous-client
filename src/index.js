a1e2f25436b6313a187d9ea8e705bc6694fbe9d7
soprajab <soprajab@hotmail.fr>
2017-07-31 19:16:06 +0200

    create-react-app from scratch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/reactbase/App';
-import registerServiceWorker from './components/registerServiceWorker';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
